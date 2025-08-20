c7408fe4adc9d9a957e461b9fd4235313f960300
ndrew <awebrite@gmail.com>
2025-08-20 23:02:18 +0300

    some changes

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -12,14 +12,6 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-
-        response = self.client.get("/lists/the-only-list-in-the-world/")
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
 
 
 class ListViewTest(TestCase):
@@ -27,16 +19,15 @@ class ListViewTest(TestCase):
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertTemplateUsed(response, "list.html")
 
-
-
     def test_renders_input_form(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        my_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=my_list)
+        Item.objects.create(text="itemey 2", list=my_list)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
@@ -55,3 +46,32 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+
+
+class ListAndItemModelsTest(TestCase):
+    def test_saving_and_retrievin_items(self):
+        my_list = List()
+        my_list.save()
+
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.list = my_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.list = my_list
+        second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, my_list)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, my_list)
+        self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, my_list)
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Item
+from .models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -13,5 +13,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST["item_text"])
+    nulist = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=nulist)
     return redirect("/lists/the-only-list-in-the-world/")
