b2cb4789c26b87d5a2de43cef8acd6df6d32622e
ndrew <awebrite@gmail.com>
2025-08-23 12:32:00 +0300

    use lxml for more specific unit test asserts on html content

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+import lxml.html
 
 
 class HomePageTest(TestCase):
@@ -9,8 +10,10 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST" action="/lists/new">')
-        self.assertContains(response, '<input name="item_text" id="id_new_item" placeholder="Enter a to do item" />', html=True)
+        parsed = lxml.html.fromstring(response.content)
+        [form] = parsed.cssselect("form[method=POST]")
+        self.assertEqual(form.get("action"), "/lists/new")
+        [input] = form.cssselect("input[name=item_text]")
 
 
 
@@ -23,8 +26,13 @@ class ListViewTest(TestCase):
     def test_renders_input_form(self):
         mylist = List.objects.create()
         response = self.client.get(f"/lists/{mylist.id}/")
-        self.assertContains(response, f'<form method="POST" action="/lists/{mylist.id}/add_item">')
-        self.assertContains(response, '<input name="item_text" id="id_new_item" placeholder="Enter a to do item" />', html=True)
+        parsed = lxml.html.fromstring(response.content)
+        [form] = parsed.cssselect("form[method=POST]")
+        self.assertEqual(form.get("action"), f"/lists/{mylist.id}/add_item")
+        [input] = form.cssselect("input[name=item_text]")
+        inputs = form.cssselect("input")
+        self.assertIn("item_text", [input.get("name") for input in inputs])
+
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
